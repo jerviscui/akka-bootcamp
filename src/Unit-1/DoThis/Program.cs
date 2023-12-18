c1f1cecc5ce94d0adbcdc23d2643e0446782a6cb
jerviscui <cuijervis@126.com>
2023-12-18 19:58:05 +0800

    Complete Lesson 1.1

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,28 +1,31 @@
 ﻿using System;
-﻿using Akka.Actor;
+using Akka.Actor;
 
 namespace WinTail
 {
     #region Program
-    class Program
+
+    internal class Program
     {
         public static ActorSystem MyActorSystem;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             PrintInstructions();
 
             // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            var writerActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
 
+            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            var readerActor =
+                MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(writerActor)));
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            readerActor.Tell("start");
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
@@ -44,5 +47,6 @@ namespace WinTail
             Console.WriteLine("Type 'exit' to quit this application at any time.\n");
         }
     }
+
     #endregion
 }
